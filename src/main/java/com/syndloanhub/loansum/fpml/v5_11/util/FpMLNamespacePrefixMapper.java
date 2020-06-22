8ce55f4f6b2f59a49740b3865a823940f53aa668
jsissler <jsissler@gmail.com>
2020-06-22 17:23:22 -0400

    eod commit, no push

---
+++ src/main/java/com/syndloanhub/loansum/fpml/v5_11/util/FpMLNamespacePrefixMapper.java
@@ -0,0 +1,22 @@
+package com.syndloanhub.loansum.fpml.v5_11.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.xml.internal.bind.marshaller.NamespacePrefixMapper;
+
+@SuppressWarnings("restriction")
+public class FpMLNamespacePrefixMapper extends NamespacePrefixMapper {
+	private Map<String, String> nsuri2prefixMap = new HashMap<String, String>();
+
+	public void addMapping(String namespaceUri, String prefix) {
+		nsuri2prefixMap.put(namespaceUri, prefix);
+	}
+
+	@Override
+	public String getPreferredPrefix(String namespaceUri, String suggestion, boolean requirePrefix) {
+		if (nsuri2prefixMap.containsKey(namespaceUri))
+			return nsuri2prefixMap.get(namespaceUri);
+		return suggestion;
+	}
+}
