3ecb57db1991de9b365a29fd5b30abeb16cefdfe
jsissler <jsissler@gmail.com>
2020-06-26 17:27:42 -0400

    first contract generated

--- src/main/java/com/syndloanhub/loansum/fpml/v5_11/util/DoubleAdapter.java
+++ src/main/java/com/syndloanhub/loansum/fpml/v5_11/util/DoubleAdapter.java
@@ -1,11 +1,21 @@
 package com.syndloanhub.loansum.fpml.v5_11.util;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
 public class DoubleAdapter {
+	public static DecimalFormat df = new DecimalFormat("0", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
+
+	static {
+		df.setMaximumFractionDigits(340);
+	}
+
 	public static double parse(String s) {
 		return Double.parseDouble(s);
 	}
 
 	public static String print(double d) {
-		return Double.toString(d);
+		return df.format(d);
 	}
 }
